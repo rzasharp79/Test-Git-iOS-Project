26ac5cabb2d7d317b62fea697f8dec92b8cd19ea
James Todd <jamestodd@Jamess-MacBook-Pro.local>
2014-08-01 06:57:48 -0400

    added a test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by James Todd on 8/1/14.
+//  Copyright (c) 2014 James Todd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by James Todd on 8/1/14.
+//  Copyright (c) 2014 James Todd. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
